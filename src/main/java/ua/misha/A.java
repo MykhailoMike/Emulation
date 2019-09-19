93c326071187dcab871f03370123452d1fcb0c1f
MykhailoMike <mkaloshyn@gmail.com>
2019-09-20 00:35:06 +0300

    A Class changes 6

--- src/main/java/ua/misha/A.java
+++ src/main/java/ua/misha/A.java
@@ -6,5 +6,6 @@ public class A {
         System.out.println("Print1 method from A class NEW!!!!!");
         System.out.println("Test");
         System.out.println("Test2");
+        System.out.println("Test3");
     }
 }
