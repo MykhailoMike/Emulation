649ee6e115a9ff690f5f3ff97cbf992138242302
MykhailoMike <mkaloshyn@gmail.com>
2019-09-19 17:46:49 +0300

    Class A change2

--- src/main/java/ua/misha/A.java
+++ src/main/java/ua/misha/A.java
@@ -3,6 +3,6 @@ package ua.misha;
 public class A {
 
     public static void print1(){
-        System.out.println("Print1 method from A class NEW!!!");
+        System.out.println("Print1 method from A class NEW!");
     }
 }
